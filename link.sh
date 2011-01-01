ccc3202d5cf1f6e535e100bd49d481150f540dc6
Michael Stum <git@stum.de>
2011-01-01 04:02:16 -0800

    link.sh checks if file exists before blindly trying to link

--- link.sh
+++ link.sh
@@ -1,8 +1,10 @@
 #! /bin/zsh
 # Create Symlinks to keep all files in the dotfiles folder
-ln -s `pwd`/.vim ~/.vim
-ln -s `pwd`/.vimrc ~/.vimrc
+[[ ! -a ~/.gitconfig ]] && ln -s `pwd`/.gitconfig ~/.gitconfig
 
-ln -s `pwd`/.zshrc ~/.zshrc
-ln -s `pwd`/.zshenv ~/.zshenv
-ln -s `pwd`/.zsh ~/.zsh
+[[ ! -a ~/.vim ]] && ln -s `pwd`/.vim ~/.vim
+[[ ! -a ~/.vimrc ]] && ln -s `pwd`/.vimrc ~/.vimrc
+
+[[ ! -a ~/.zshrc ]] && ln -s `pwd`/.zshrc ~/.zshrc
+[[ ! -a ~/.zshenv ]] && ln -s `pwd`/.zshenv ~/.zshenv
+[[ ! -a ~/.zsh ]] && ln -s `pwd`/.zsh ~/.zsh
